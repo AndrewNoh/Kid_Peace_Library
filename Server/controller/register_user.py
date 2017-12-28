07f1f3ad935fcf7bdd1f5e06b424b6a873cd3312
AndrewNoh <notho0@naver.com>
2017-12-28 12:16:15 +0900

    modify ver0.2

--- Server/controller/register_user.py
+++ Server/controller/register_user.py
@@ -5,6 +5,7 @@ from Server.app_blueprint import app
 from Server.model.user import user
 from Server.database import DB
 from collections import OrderedDict
+from Server.controller.login import login_requied
 
 @app.route('/user/check_id')
 def Check_id():
@@ -51,14 +52,31 @@ def signup_form():
     return redirect(url_for('.index'))
 
 
-@app.route('/modify', methods=['post'])
-	def modify():
-		get_user = login_requied()
-		if request.method == 'POST'
-    		id = get_user.id
-            email = request.form['email']
-            password = request.form['password']
-            name = request.form['name']
-            cell_phone = request.form['cell_phone']
+@app.route('/user/modify')
+def modify():
+    get_user = login_requied
+    if request.method == 'POST':
+        id = get_user.id
+        email = request.form['email']
+        password = request.form['password']
+        name = request.form['name']
+        cell_phone = request.form['cell_phone']
+        new_passowrd = request.form['new_password']
+        if password == '':
+            return render_template('alert_msg.html', msg="회원가입 실패! 비밀번호는 8자리 이상 문자, 숫자, 특수문자로 구성하여야 합니다.")
+        elif password == get_user.password :
+            item = user(\
+                        id = id,
+                        permission = 'user',
+                        cell_phone = cell_phone,
+                        email = email,
+                        name = name,
+                        password = new_password)
+            mydb = DB()
+            if not mydb.modify(item):
+                del mydb
+                return render_template('alert_msg.html', msg="회원정보 수정 실패")
+            del mydb
+        return redirect(url_for('.index'))
 
-        return render_template('modify.html', name = get_user.name, password = get_user.password, cell_phone = get_user.cell_phone, id = get_user.id, email = get_user.email)
+    return render_template('modify.html', name정 = get_user.name, password = get_user.password, cell_phone = get_user.cell_phone, id = get_user.id, email = get_user.email)
